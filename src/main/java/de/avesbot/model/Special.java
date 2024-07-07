3da4c4647e83c4bec1ca9cd27672cc9c688338dc
Nitrout <nitrout@ala-alba.de>
2024-07-08 00:23:30 +0200

    UPDATE reworked data classes

--- src/main/java/de/avesbot/model/Special.java
+++ src/main/java/de/avesbot/model/Special.java
@@ -1,22 +1,10 @@
 package de.avesbot.model;
 
-import java.util.Optional;
-
 /**
  * Represents a special ability of a character.
  * @author Nitrout
  */
-public record Special(String name, Optional<String> attribute1, Optional<String> attribute2, Optional<String> attribute3) {
+public record Special(String name, String attribute1, String attribute2, String attribute3) {
 	
-	/**
-	 * Creates a new special ability.
-	 * 
-	 * @param name the name of the special ability
-	 * @param attribute1 the first optional attribute
-	 * @param attribute2 the second optional attribute
-	 * @param attribute3 the third optional attribute
-	 */
-	public Special(String name, String attribute1, String attribute2, String attribute3) {
-		this(name, Optional.ofNullable(attribute1), Optional.ofNullable(attribute2), Optional.ofNullable(attribute3));
-	}
+
 }
--- src/main/java/de/avesbot/model/Vantage.java
+++ src/main/java/de/avesbot/model/Vantage.java
@@ -1,21 +1,9 @@
 package de.avesbot.model;
 
-import java.util.Optional;
-
 /**
  * Represents a (dis)advantage of a character an its optional attributes.
  * @author Nitrout
  */
-public record Vantage(String name, Optional<String> attribute1, Optional<String> attribute2) {
+public record Vantage(String name, String attribute1, String attribute2) {
 	
-	/**
-	 * Creates a new vantage.
-	 * 
-	 * @param name the name of the vantage
-	 * @param attribute1 the first optional attribute
-	 * @param attribute2 the second optional attribute
-	 */
-	public Vantage(String name, String attribute1, String attribute2) {
-		this(name, Optional.ofNullable(attribute1), Optional.ofNullable(attribute2));
-	}
 }
