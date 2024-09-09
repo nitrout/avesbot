11cde075e8523522bc99c53d5149822ee4686cc7
Nitrout <nitrout@ala-alba.de>
2024-09-09 19:54:50 +0200

    REFACTOR

--- src/main/java/de/avesbot/callable/group/GroupAttributeCallable.java
+++ src/main/java/de/avesbot/callable/group/GroupAttributeCallable.java
@@ -9,7 +9,6 @@ import de.avesbot.model.Attribute;
 import de.avesbot.model.Group;
 import de.avesbot.model.RoleplayCharacter;
 import de.avesbot.callable.RoleplayCharacterRoll;
-import de.avesbot.i18n.I18n;
 import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
@@ -23,17 +22,16 @@ import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 public class GroupAttributeCallable extends GroupCallable implements RoleplayCharacterRoll {
 	
 	private final HashMap<String, Emoji> emoteMap = new HashMap<>();
-	protected static final I18n I18N = GroupCallable.I18N;
 	
 	public static final SubcommandData SUBCOMMAND;
 	
 	static {
-		SUBCOMMAND = buildTranslatedSubcommand(I18N, "groupAttribute", "groupAttributeDescription");
+		SUBCOMMAND = buildTranslatedSubcommand(GroupCallable.I18N, "groupAttribute", "groupAttributeDescription");
 		
-		OptionData attributeOpption = buildTranslatedOption(I18N, OptionType.STRING, "attributeOption", "attributeOptionDescription", true);
+		OptionData attributeOpption = buildTranslatedOption(GroupCallable.I18N, OptionType.STRING, "attributeOption", "attributeOptionDescription", true);
 		attributeOpption.addChoices(Attribute.OPTION_CHOICES);
 		
-		OptionData difficultyOption = buildTranslatedOption(I18N, OptionType.INTEGER, "difficultyOption", "difficultyOptionDescription", false);
+		OptionData difficultyOption = buildTranslatedOption(GroupCallable.I18N, OptionType.INTEGER, "difficultyOption", "difficultyOptionDescription", false);
 		
 		SUBCOMMAND.addOptions(attributeOpption, difficultyOption);
 	}
@@ -54,23 +52,22 @@ public class GroupAttributeCallable extends GroupCallable implements RoleplayCha
 		
 		LinkedList<String> results = new LinkedList<>();
 		Optional<Group> group = Avesbot.getStatementManager().getUsersActiveGroup(guild, member);
-		Optional<Attribute> attribute = Optional.empty();
 		byte difficulty = 0;
 		
-		attribute = Optional.of(Attribute.valueOf(this.commandPars.get("attribute").getAsString().toUpperCase()));
+		var attribute = Optional.of(Attribute.valueOf(this.commandPars.get("attribute").getAsString().toUpperCase()));
 		if(this.commandPars.containsKey("difficulty"))
 			difficulty = (byte)this.commandPars.get("difficulty").getAsLong();
 		
 		if(group.isEmpty()) {
-			return I18N.getTranslation(settings.locale(), "errorNoActiveGroup");
+			return GroupCallable.I18N.getTranslation(settings.locale(), "errorNoActiveGroup");
 		} else if(attribute.isEmpty()) {
-			return I18N.getTranslation(settings.locale(), "errorNoAttribute");
+			return GroupCallable.I18N.getTranslation(settings.locale(), "errorNoAttribute");
 		} else {
 			
 			RoleplayCharacter[] charas = Avesbot.getStatementManager().getGroupMemberList(group.get());
 			
 			if(charas.length == 0)
-				return I18N.getTranslation(settings.locale(), "errorNoGroupMember");
+				return GroupCallable.I18N.getTranslation(settings.locale(), "errorNoGroupMember");
 			
 			for(RoleplayCharacter chara : charas) {
 				results.add(rollAttribute(settings, emoteMap, chara, attribute.get(), difficulty));
